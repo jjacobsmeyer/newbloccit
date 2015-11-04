7dea002e1aed902ec7c56928412a8a84aeac2ff5
jjacobsmeyer <jamesjacobsmeyer@gmail.com>
2015-11-04 15:11:58 -0600

    Assignment 36 completed, Authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase }
+  before_save :reformatted_name
+
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -12,5 +14,16 @@ class User < ActiveRecord::Base
     format: { with: EMAIL_REGEX }
 
   has_secure_password
-  
+
+  def reformatted_name
+    if name
+    name_array = []
+    name.split.each do |first_last|
+      name_array << first_last.capitalize
+    end
+
+    self.name = name_array.join(" ")
+  end
+  end
+
 end
