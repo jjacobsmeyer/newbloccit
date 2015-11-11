95c6c13874d34fbfaaf0c72d1d20f267e6fe3ca6
jjacobsmeyer <jamesjacobsmeyer@gmail.com>
2015-11-11 09:53:36 -0600

    Assignment 39 completed, Posts and Users

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,14 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { reorder( title: :asc) }
+  scope :ordered_by_reverse_created_at, -> { reorder('created_at ASC') }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+
 
 end
