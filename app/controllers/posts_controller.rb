7ba4b985917b2cd85bad7123fd0fc017aa92b04f
jjacobsmeyer <jamesjacobsmeyer@gmail.com>
2015-11-17 19:57:39 -0600

    Checkpoint 42 completed, Labels

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
     @post.user = current_user
 
       if @post.save
+        @post.labels = Label.update_labels(params[:post][:labels])
         flash[:notice] = "Post was saved."
         redirect_to [@topic, @post]
       else
@@ -35,6 +36,7 @@ class PostsController < ApplicationController
     @post.assign_attributes(post_params)
 
     if @post.save
+      @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = "Post was updated."
       redirect_to [@post.topic, @post]
     else
@@ -69,6 +71,6 @@ class PostsController < ApplicationController
       redirect_to [post.topic, post]
     end
   end
-  
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :labels, only: [:show]
+
   resources :topics do
     resources :posts, except: [:index]
   end
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
   end
-  
+
 
   get 'about' => 'welcome#about'
 
