a97246245b3f746378e2c924142eb61f75164c26
jjacobsmeyer <jamesjacobsmeyer@gmail.com>
2015-11-23 08:49:52 -0600

    Checkpoint 47 completed, Ajax

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,6 +7,8 @@ class Comment < ActiveRecord::Base
 
   after_create :send_favorite_emails
 
+  default_scope { order('updated_at DESC') }
+
   private
 
   def send_favorite_emails
@@ -14,5 +16,5 @@ class Comment < ActiveRecord::Base
       FavoriteMailer.new_comment(favorite.user, post, self).deliver_now
     end
   end
-  
+
 end
